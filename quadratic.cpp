3db763fbb5a61932538171ca062cf669d892d198
TerentevVSs <terentev.vs@phystech.edu>
2022-08-19 18:55:46 +0300

    added header

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,15 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-
-const double eps = 1e-10;
-
-bool check_str(char *a, const char *b);
-
-double read();
-
-void solve(double a, double b, double c);
+#include "qudratic.h"
 
 int main()
 {
---
+++ qudratic.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+const double eps = 1e-10;
+
+bool check_str(char *a, const char *b);
+
+double read();
+
+void solve(double a, double b, double c);
