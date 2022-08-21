882a53bf4f777733108e066b4db1d75b124afdf3
TerentevVSs <terentev.vs@phystech.edu>
2022-08-21 19:50:25 +0300

    fix test

--- quadratic/tests.cpp
+++ quadratic/tests.cpp
@@ -65,7 +65,7 @@ int main()
                  .c=12},
              {
                  .x1=-2,
-                 .x2=-37,
+                 .x2=-3,
                  .rootCount=twoSolutions,
              }
             },
