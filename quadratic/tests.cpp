0dd7d4a80ef40003dd86f5f43c762d972b262a4f
TerentevVSs <terentev.vs@phystech.edu>
2022-08-21 19:53:10 +0300

    refactoring

--- quadratic/tests.cpp
+++ quadratic/tests.cpp
@@ -62,15 +62,15 @@ int main()
             },
             {
                 {
-                 .a = 2,
-                 .b = 10,
-                 .c = 12
-             },
-             {
-                 .x1 = -2,
-                 .x2 = -3,
-                 .rootCount = twoSolutions,
-             }
+                    .a = 2,
+                    .b = 10,
+                    .c = 12
+                },
+                {
+                    .x1 = -2,
+                    .x2 = -3,
+                    .rootCount = twoSolutions,
+                }
             },
         };
 
